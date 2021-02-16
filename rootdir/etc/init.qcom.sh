e0a8361d90eaf6aa473b7f1a5e7c451aa0f47f18
Undying-yueyue <orgtech007@gmail.com>
2021-02-16 00:16:50 -0800

    Modify for exTHmUI

---
+++ rootdir/etc/init.qcom.sh
@@ -0,0 +1,58 @@
+#!/vendor/bin/sh
+# Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of The Linux Foundation nor
+#       the names of its contributors may be used to endorse or promote
+#       products derived from this software without specific prior written
+#       permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# Set shared buttons and touchpanel nodes ownership (these are proc_symlinks to the real sysfs nodes)
+chown -LR system.system /proc/buttons
+chown -L system.system /proc/touchpanel/reversed_keys_enable
+chown -L system.system /proc/touchpanel/capacitive_keys_enable
+
+chmod g+w -R /data/vendor/radio/modem_config/*
+rm -rf /data/vendor/radio/modem_config/*
+cp -rf /system/vendor/mbn/* /data/vendor/radio/modem_config/
+chown -hR radio.root /data/vendor/radio/modem_config/*
+if [ -f /system/vendor/mbn/mbn_ota.txt ] && [ ! -f /data/vendor/radio/modem_config/mbn_ota.txt ]; then
+    cp /system/vendor/mbn/mbn_ota.txt /data/vendor/radio/modem_config/
+    chown radio.root /data/vendor/radio/modem_config/mbn_ota.txt
+fi
+
+chmod g-w /data/vendor/radio/modem_config
+setprop ro.vendor.ril.mbn_copy_completed 1
+
+# Check build variant for printk logging
+# Current default minimum boot-time-default
+buildvariant=`getprop ro.build.type`
+case "$buildvariant" in
+    "userdebug" | "eng")
+        #set default loglevel to KERN_INFO
+        echo "6 6 1 7" > /proc/sys/kernel/printk
+        ;;
+    *)
+        #set default loglevel to KERN_WARNING
+        echo "4 4 1 4" > /proc/sys/kernel/printk
+        ;;
+esac
